b80182b7c6aa7e08a10009a658eba296534cc652
Hernan <h.de.prada1@gmail.com>
2020-02-17 13:10:47 -0300

    Aplied changes requested by Abel

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,45 +1,45 @@
-import React, {Component} from "react";
+/* global alert */
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import "./Welcome.css"
-export default class SignIn extends Component{
+import './Welcome.css'
+export default class SignIn extends Component {
   state = {
     username: null,
     password: null,
     firstname: null,
     lastname: null
   }
-  
-  constructor(){
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+
+  constructor () {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange = (e) =>{
+  handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
-    });
+    })
   }
 
   handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state.username, this.state.password);
-    return(
-      alert("Logeado")
+    e.preventDefault()
+    return (
+      alert('Logeado')
     )
   }
 
-  render(){
+  render () {
     return (
-      <div className="outer">
-        <div className="middle">
-          <div className="inner">
-            <div className="wrapper fadeInDown">
-              <div id="formContent">
+      <div className='outer'>
+        <div className='middle'>
+          <div className='inner'>
+            <div className='wrapper fadeInDown'>
+              <div id='formContent'>
                 <h1>Sign In</h1>
                 <form onSubmit={this.handleSubmit}>
-                  <input type="text" id="username" className="fadeIn first" placeholder="Username" onChange={this.handleChange}/>
-                  <input type="password" id="password" className="fadeIn second" placeholder="Password" onChange={this.handleChange}/>
-                  <button type="submit" className="fadeIn third" onClick={this.handleSubmit} >Login</button>
+                  <input type='text' id='username' className='fadeIn first' placeholder='Username' onChange={this.handleChange} />
+                  <input type='password' id='password' className='fadeIn second' placeholder='Password' onChange={this.handleChange} />
+                  <button type='submit' className='fadeIn third' onClick={this.handleSubmit}>Login</button>
                 </form>
                 <Link to='/auth/signup'>
                   Don't have an account? Create one!
@@ -52,6 +52,3 @@ export default class SignIn extends Component{
     )
   }
 }
-
-
-
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,47 +1,46 @@
-import React, {Component} from "react";
+/* global alert */
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import "./Welcome.css"
-export default class SignUp extends Component{
-  state = {
-    username: null,
-    password: null,
-    firstname: null,
-    lastname: null
-  }
-  
-  constructor(){
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+import './Welcome.css'
+export default class SignUp extends Component {
+  constructor () {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      username: null,
+      password: null,
+      firstname: null,
+      lastname: null
+    }
   }
 
-  handleChange = (e) =>{
+  handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
-    });
+    })
   }
 
   handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state.username, this.state.password, this.state.firstname, this.state.lastname);
-    return(
-      alert("Usuario Creado")
+    e.preventDefault()
+    return (
+      alert('Usuario Creado')
     )
   }
 
-  render(){
+  render () {
     return (
-      <div className="outer">
-        <div className="middle">
-          <div className="inner">
-            <div className="wrapper fadeInDown">
-              <div id="formContent">
+      <div className='outer'>
+        <div className='middle'>
+          <div className='inner'>
+            <div className='wrapper fadeInDown'>
+              <div id='formContent'>
                 <h1>Member Registration</h1>
                 <form onSubmit={this.handleSubmit}>
-                  <input type="text" id="firstname" className="fadeIn first" placeholder="FirstName" onChange={this.handleChange}/>
-                  <input type="text" id="lastname" className="fadeIn second" placeholder="LastName" onChange={this.handleChange}/>
-                  <input type="text" id="username" className="fadeIn third" placeholder="Username" onChange={this.handleChange}/>
-                  <input type="password" id="password" className="fadeIn fourth" placeholder="Password" onChange={this.handleChange}/>
-                  <button type="submit" className="fadeIn third" onClick={this.handleSubmit} >Sign up</button>
+                  <input type='text' id='firstname' className='fadeIn first' placeholder='FirstName' onChange={this.handleChange} />
+                  <input type='text' id='lastname' className='fadeIn second' placeholder='LastName' onChange={this.handleChange} />
+                  <input type='text' id='username' className='fadeIn third' placeholder='Username' onChange={this.handleChange} />
+                  <input type='password' id='password' className='fadeIn fourth' placeholder='Password' onChange={this.handleChange} />
+                  <button type='submit' className='fadeIn third' onClick={this.handleSubmit}>Sign up</button>
                 </form>
                 <Link to='/auth/signin'>
                   Do you already have a account?
